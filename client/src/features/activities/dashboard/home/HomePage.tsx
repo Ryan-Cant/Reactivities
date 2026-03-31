fc92bf87dd873fa7b43f925281fccaa3930e1701
Ryan Cant <rcant@singular.co.za>
2026-03-31 11:10:55 +0200

    End of Section 8

--- client/src/features/activities/dashboard/home/HomePage.tsx
+++ client/src/features/activities/dashboard/home/HomePage.tsx
@@ -1,9 +1,43 @@
-import { Container, Typography } from '@mui/material'
+import { Group } from "@mui/icons-material";
+import { Box, Button, Paper, Typography } from "@mui/material";
+import { Link } from "react-router";
 
 export default function HomePage() {
     return (
-        <Container sx={{ mt: 3 }}>
-            <Typography variant= "h3">Home Page</Typography>
-        </Container>
-    )
+        <Paper
+            sx={{
+                color: "white",
+                display: "flex",
+                flexDirection: "column",
+                gap: 6,
+                alignItems: "center",
+                justifyContent: "center",
+                height: "100vh",
+                backgroundImage:
+                    "linear-gradient(135deg, #182a73 0%, #218aae 69%, #20a7ac 89%)",
+            }}
+        >
+            {/* Icon + Title */}
+            <Box sx={{ display: "flex", alignItems: "center", gap: 3 }}>
+                <Group sx={{ height: 110, width: 110 }} />
+                <Typography variant="h1">Reactivities</Typography>
+            </Box>
+
+            {/* Subtitle */}
+            <Typography variant="h4">
+                Welcome to Reactivities
+            </Typography>
+
+            {/* Button */}
+            <Button
+                component={Link}
+                to="/activities"
+                size="large"
+                variant="contained"
+                sx={{ height: 80, borderRadius: 4, fontSize: "1.5rem" }}
+            >
+                Take me to Reactivities
+            </Button>
+        </Paper>
+    );
 }
